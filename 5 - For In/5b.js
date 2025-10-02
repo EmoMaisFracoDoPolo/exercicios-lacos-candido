75c536efcb63193dd29ee7f03717bbe6d05837f8
Maior fã de MCR <guilherme.candido.farias@escola.pr.gov.br>
2025-10-02 09:44:53 -0300

    Add files via upload

--- 5 - For In/5b.js
+++ 5 - For In/5b.js
@@ -2,5 +2,5 @@ let pessoa = {nome: "Ana", idade: 25, cidade: "Curitiba"};
 
 
 for(let chave in pessoa){
-    console.log(chave.valueOf());
+    console.log(pessoa[chave]);
 }
---
+++ 5 - For In/5c.js
@@ -0,0 +1,6 @@
+
+let carro = {marca: "Toyota", nome: "Corolla", ano: 2020};
+
+for(let info in carro){
+  console.log(info, " : ", carro[info]);
+}
